12fbd25afdcf10fce1d6c0ead879a60402d3fd69
EgorEast <jir.jir35@gmail.com>
2021-08-13 12:32:36 +0300

    navigation for routing is added

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,11 @@
+import Router from 'next/router';
+
 export default function About() {
-	return <h1>About Page</h1>;
+	return (
+		<>
+			<h1>About Page</h1>
+			<button onClick={() => Router.push('/')}>Go back to home</button>
+			<button onClick={() => Router.push('/posts')}>Go to posts</button>
+		</>
+	);
 }
