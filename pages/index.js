061c546494bda1fd47bc5ee95b0d2c32862fee3c
EgorEast <jir.jir35@gmail.com>
2021-08-13 13:00:52 +0300

    meta and seo for start page is added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Index() {
 	return (
 		<>
+			<Head>
+				<title>Start Page Next</title>
+				<meta name='keywords' content='next,javascript,nextjs,react' />
+				<meta
+					name='description'
+					content="this is educational project making to Minin's tutorial for next.js"
+				/>
+				<meta charSet='utf-8' />
+			</Head>
 			<h1>Helo Next.JS</h1>
 			<p>
 				<Link href={'/about'}>
--- pages/posts.js
+++ pages/posts.js
@@ -1,5 +1,14 @@
+import Head from 'next/head';
+
 export default function Posts() {
-	return <h1>Posts Page</h1>;
+	return (
+		<>
+			<Head>
+				<title>Posts Page Next</title>
+			</Head>
+			<h1>Posts Page</h1>
+		</>
+	);
 }
 
 // /posts
