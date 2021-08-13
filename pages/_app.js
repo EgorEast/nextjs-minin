5802f9b6b1cbc851c15130b4e6c82bddfdd9942b
EgorEast <jir.jir35@gmail.com>
2021-08-13 18:46:58 +0300

    remove comment in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,6 @@ export default function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Component {...pageProps} />
-			{/* <style jsx global>{`
-				body {
-					font-family: 'Roboto', sans-serif;
-				}
-			`}</style> */}
 		</>
 	);
 }
