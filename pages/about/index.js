807474533b9e3fdd1ae2c5df79cd806ab6a09a22
EgorEast <jir.jir35@gmail.com>
2021-08-14 15:43:19 +0300

    loading idetifier is added

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,12 +1,21 @@
 import Router from 'next/router';
 import { MainLayout } from '../../components/MainLayout';
 
-export default function About() {
+export default function About({ title }) {
 	return (
 		<MainLayout title={'About Page'}>
-			<h1>About Page</h1>
+			<h1>{title}</h1>
 			<button onClick={() => Router.push('/')}>Go back to home</button>
 			<button onClick={() => Router.push('/posts')}>Go to posts</button>
 		</MainLayout>
 	);
 }
+
+About.getInitialProps = async () => {
+	const responce = await fetch('http://localhost:4200/about');
+	const data = await responce.json();
+
+	return {
+		title: data.title,
+	};
+};
